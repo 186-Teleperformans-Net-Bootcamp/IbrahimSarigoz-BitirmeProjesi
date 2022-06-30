8c1d32a050dd97e1049995afc23c40502da8ec28
ibrahim sarıgöz <srgzibrahim@gmail.com>
2022-06-30 17:42:31 +0300

    Repository interfacelerini yazdım.

---
+++ ShoppingList.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingList.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Application.Repositories
+{
+    public interface IReadRepository <T>: IRepository<T>  where T:BaseEntity
+    {
+        // sorguda çalıştığım için Iqueryable diyorum. 
+        IQueryable<T> GetAll();
+
+        IQueryable<T> GetWhere(Expression<Func<T,bool>> expression);
+
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> expression); // Async çalıştığı için datamızı Task olarak döndürdük. 
+
+        Task<T> GetByIdAsync(string id); // ORM de Async çalıştıkları için böyle kodladım. 
+
+    }
+}
