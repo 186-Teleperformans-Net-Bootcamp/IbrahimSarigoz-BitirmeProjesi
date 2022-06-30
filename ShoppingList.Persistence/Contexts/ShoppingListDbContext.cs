d6a7b67b652c43d571ed07f4e59ba1021150882e
ibrahim sarıgöz <srgzibrahim@gmail.com>
2022-06-30 23:50:08 +0300

    SaveChangesAsync üzerinde bir override işlemi yaptım. Bu sayede her eklenen yeni bir entity e id otomatik oluşturulcak.

--- ShoppingList.Persistence/Contexts/ShoppingListDbContext.cs
+++ ShoppingList.Persistence/Contexts/ShoppingListDbContext.cs
@@ -19,5 +19,26 @@ namespace ShoppingList.Persistence.Contexts
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<ShopList> ShopLists { get; set; }
+
+        //burda her yeni entity oluştuğunda ona id atmak için yapıyorum.
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //yapılan değişiklikleri yakalıyorum.
+            var items = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var item in items)
+            {
+                //artık add fonksiyonu her çağırıldığımda yeni bir id ekliyor otomatik 
+                if (EntityState.Added==item.State )
+                {
+                    item.Entity.Id = Guid.NewGuid();
+
+                }
+             
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
     }
 }
