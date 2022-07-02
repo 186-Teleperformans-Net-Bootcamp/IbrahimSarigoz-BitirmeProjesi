b5f1c45f029719a0143a1ca982b683460bf9f827
ibrahim sarıgöz <srgzibrahim@gmail.com>
2022-07-02 15:29:28 +0300

    Validation işlemleri için bir şeyler kodladım.

---
+++ ShoppingList.Application/Validators/ShopLists/CreateShopListValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using ShoppingList.Application.ViewModels.ShopLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Application.Validators.ShopLists
+{
+    public class CreateShopListValidator : AbstractValidator<VM_Create_ShopList>
+    {
+        public CreateShopListValidator()
+        {
+            RuleFor(p => p.ListName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Please enter the list name correctly")
+                .MaximumLength(80)
+                .MinimumLength(5)
+                .WithMessage("Name of the list should be 5-80 characters");
+
+            RuleFor(p => p.IsItCompleted).NotNull().NotEmpty().WithMessage("ShopList can be completed or not");
+        }
+
+
+    }
+}
