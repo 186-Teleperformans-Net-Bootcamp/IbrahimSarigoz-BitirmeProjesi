18f4116dd47ccb4e118454db95fac94aff2822dc
ibrahim sarıgöz <srgzibrahim@gmail.com>
2022-07-05 13:13:28 +0300

    Kullanıcı oluşmadığında error fırlatıyorum.

---
+++ ShoppingList.Application/Exceptions/UserCreateFailException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Application.Exceptions
+{
+    public class UserCreateFailException :Exception
+    {
+        public UserCreateFailException() : base ("Something went wrong when you try to create a user")
+        {
+
+        }
+
+
+    }
+}
